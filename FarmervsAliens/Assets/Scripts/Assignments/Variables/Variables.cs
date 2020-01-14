a14672aed3020f51bc7e796e98804d040563e7f5
Preston Powell <Preston.powell@uvu.edu>
2020-01-13 17:15:45 -0700

    Created Level0 scene and a variable script

---
+++ FarmervsAliens/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // Declaration 
+    int cookieBox; 
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       cookieBox = 20;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
