625dd9a3801ac8c812c0e4866a66ad23649fe50e
Preston Powell <Preston.powell@uvu.edu>
2020-01-27 17:15:20 -0700

    Colliders and triggers

--- FarmervsAliens/Assets/Scripts/Assignments/Variables/Move.cs
+++ FarmervsAliens/Assets/Scripts/Assignments/Variables/Move.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public float speed;
+    public float verticleInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +17,30 @@ public class Move : MonoBehaviour
     void Update()
     {
         transform.Translate(0, 0, 0.1f);
+                        //  ( x, y, z)
     }
 
     // Detect collison with another object
     void OnCollisionEnter(Collision other){
+       
+
+        if (other.gameObject.CompareTag("Floor"))//Primary
+        {
+            Debug.Log("Colliding with Floor");
+        }
+        else if(other.gameObject.CompareTag("Obstacle"))// Secondary
+        {
+            Debug.Log("Colliding with Obstacle");
+        }
+        else // Default
+        {
+            Debug.Log("...");
+        }
 
     }
 
     void OnTriggerEnter(Collider other){
-
+        Debug.Log("You have entered the trigger!");
     }
 
 }
