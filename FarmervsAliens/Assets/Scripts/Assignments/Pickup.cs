882745167590e5138263176fff644e1691db3031
Preston Powell <Preston.powell@uvu.edu>
2020-02-05 17:36:08 -0700

    Added pickup

---
+++ FarmervsAliens/Assets/Scripts/Assignments/Pickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public string pickupName;
+    public string pickupType;
+    public int pointsToAdd;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.AddPoints(pointsToAdd);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ FarmervsAliens/Assets/Scripts/Assignments/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public static int score;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void AddPoints(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        Debug.Log("Game Score: " + score);
+
+    }
+}
