24fde9948398862ca555a3de442014c89c7b9ca0
Preston Powell <Preston.powell@uvu.edu>
2020-02-26 17:30:47 -0700

    Added Enemy Health and Bullet scripts

---
+++ FarmervsAliens/Assets/Scripts/Assignments/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage = 1;
+    public int time = 5;
+
+    void Start()
+    {
+        StartCoroutine(DestroyBullet());
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            var hit = other.gameObject;
+            var health = hit.GetComponent<EnemyHealth>();
+
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+                Debug.Log("Ouch, you hit me!");
+            }
+            
+        }
+        
+    }
+    
+    IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+
+
+}
+
---
+++ FarmervsAliens/Assets/Scripts/Assignments/EnemyHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth = 3;
+    public Transform spawnPoint;
+    public int points = 10;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            //Keep score at zero
+            currentHealth = 0;
+            print("Enemy is Dead!");
+            // Add points to score for killing enemy
+            //scoreManager.AddPoints(points);
+            //Move enemy to spawn point for restart
+            transform.position = spawnPoint.position;
+            transform.rotation = spawnPoint.rotation;
+            //Reset enemy Health
+            currentHealth = maxHealth;
+        }
+    }
+}
