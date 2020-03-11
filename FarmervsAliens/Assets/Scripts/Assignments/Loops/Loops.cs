119687f114703a1b018b6adb7fa8de264ec2e965
Preston Powell <Preston.powell@uvu.edu>
2020-03-11 17:36:51 -0600

    Update 3/11

--- FarmervsAliens/Assets/Scripts/Assignments/Loops/Loops.cs
+++ FarmervsAliens/Assets/Scripts/Assignments/Loops/Loops.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Loops : MonoBehaviour
 {
     //This variable is used with the For loop example
-    public int numEnemies = 5;
+    //public int numEnemies = 5;
     //This variable is used with the While loop example
-    public int cupsInTheSink = 10;
+    //public int cupsInTheSink = 10;
 
 
 
@@ -15,26 +15,26 @@ public class Loops : MonoBehaviour
     void Start()
     {
         //For Loop Example
-        for (int i = 0; i < numEnemies; i++)
+        /*for (int i = 0; i <= numEnemies; i++)
         {
             Debug.Log("Creating enemy number: " + i);
-        }
+        }*/
 
         //While Loop Example
-        while (cupsInTheSink > 0)
+      /* while (cupsInTheSink > 0)
         {
             Debug.Log("I've washed a cup!");
             cupsInTheSink--;
-        }
+        }*/
 
         //Do While Loop Example
-        bool shouldContinue = false;
+       /*bool shouldContinue = false;
 
         do
         {
             print("Hello World");
 
-        } while (shouldContinue == true);
+        } while (shouldContinue == true);*/
 
         //Foreach Loop Example
         string[] strings = new string[3];
--- FarmervsAliens/Assets/Scripts/Assignments/SpawnManager.cs
+++ FarmervsAliens/Assets/Scripts/Assignments/SpawnManager.cs
@@ -5,35 +5,49 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
-    public float xSpawnRange;
-    public float zSpawnPos;
-    public float spawnInterval;
-    public float startDelay;
+    //public float xSpawnRange;
+    //public float zSpawnPos;
+    //public float spawnInterval;
+    //public float startDelay;
     public int enemyIndex;
+
+    public int enemiesToSpawn;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomEnemy", startDelay, spawnInterval );
+        //InvokeRepeating("SpawnRandomEnemy", startDelay, spawnInterval );
+
+        SpawnEnemyWave();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
 
-        if (Input.GetKeyDown(KeyCode.E))
+
+        /*if (Input.GetKeyDown(KeyCode.E))
         {
             SpawnRandomEnemy();
-        } 
+        } */
     }
 
-    void SpawnRandomEnemy(){
+    /*void SpawnRandomEnemy(){
         Vector3 spawnpos = new Vector3(Random.Range(-xSpawnRange, xSpawnRange), 0, zSpawnPos);
 
          int enemyIndex = Random.Range(0, enemyPrefabs.Length);
 
         Instantiate(enemyPrefabs[enemyIndex], spawnpos, enemyPrefabs[enemyIndex].transform.rotation);
+    }*/
+
+    void SpawnEnemyWave()
+    {
+        for( int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefabs[enemyIndex], transform.position, enemyPrefabs[enemyIndex].transform.rotation);
+        }
     }
+
+   
 }
