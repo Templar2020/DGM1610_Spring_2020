7ba9cb2a763eccd69ec38f49955259f53bae4505
Preston Powell <preston.powell2020@gmail.com>
2020-04-08 17:30:08 -0600

    Added Wander AI and Scriptable Objects

---
+++ FvA/Assets/Scripts/Assignments/Scriptable Objects/SO.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ //This following line of code adds our Scriptable Object as an option in the Asset menu. 
+[CreateAssetMenu(fileName="New SO", menuName="Scriptable Objects/New Object")]
+
+public class SO : ScriptableObject // Notice how we change the inheritance from MonoBehavior to SciptableObject
+{
+   
+    //Scriptable Objects like Mono scripts can contain variables.
+
+    public string fullName;
+    public string description;
+
+   public GameObject model;
+
+   public Color color;
+
+    //Scriptable Objects do not use monobehavior callbacks such as Start() and Update().
+
+    //Scriptable objects can also contain functions and conditionals.
+
+    /* private void PrintOut(string name, string description){
+        Debug.Log(name + " "+ description);
+        if(name == "Chuck Norris"){
+            Debug.Log("Chuck Norris allows this program to run!");
+        }
+    }     */
+   
+}
---
+++ FvA/Assets/Scripts/Assignments/Scriptable Objects/Test_Object.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Object : MonoBehaviour
+{
+    public SO data;
+    private GameObject model;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+     model = data.model;
+
+     var modelRend = model.GetComponent<Renderer>();    
+     modelRend.sharedMaterial.SetColor("_Color",data.color);
+     
+     print(data.name);
+     print(data.description);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+  
+    }
+}
